1e740bf85574a8f3095ca0d80898fc198dbd930b
Chapman Pendery <35637443+cpendery@users.noreply.github.com>
2024-03-06 15:45:47 -0500

    fix: force > as prompt even when zsh theme exists (#17)
    * fix: force > as prompt even when theme exists
    
    Signed-off-by: Chapman Pendery <cpendery@microsoft.com>
    
    * fix: load add-zsh-hook
    
    Signed-off-by: Chapman Pendery <cpendery@microsoft.com>
    
    ---------
    
    Signed-off-by: Chapman Pendery <cpendery@microsoft.com>

--- shell/shellIntegration-rc.zsh
+++ shell/shellIntegration-rc.zsh
@@ -1,6 +1,13 @@
+builtin autoload -Uz add-zsh-hook
+
 if [[ -f $USER_ZDOTDIR/.zshrc ]]; then
 	ZDOTDIR=$USER_ZDOTDIR
 	. $USER_ZDOTDIR/.zshrc
 fi
 
+__tui_precmd() {
+	PS1="> "
+}
 PS1="> "
+
+add-zsh-hook precmd __tui_precmd
